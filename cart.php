066a33ee8e1c8cbc7df65b103f3e3da4eba72c58
Vishakha <guptavishakha84@gmail.com>
2018-04-25 20:26:04 +0530

    orders table

--- cart.php
+++ cart.php
@@ -1,5 +1,6 @@
 <?php
 include_once './staticHeaded.php';
+include_once './sql/connection.php';
 ?>
 <!DOCTYPE html>
 <html> 
@@ -9,7 +10,36 @@ include_once './staticHeaded.php';
     <body>
         <h1>Cart page called</h1> 
         <button>Checkout</button>
-           
+        
+        <!--Code to create tables if they don't exist-->
+        <?php
+            //Orders table
+            $query = $GLOBALS['$con']->query("SHOW TABLES LIKE 'ORDERS'");
+            if($query->num_rows == 0){
+                $sql = "CREATE TABLE ORDERS (
+                        orderID int NOT NULL AUTO_INCREMENT,
+                        userID int NOT NULL,
+                        address varchar(250) NOT NULL,
+                        status int NOT NULL,
+                        PRIMARY KEY (orderID),
+                        FOREIGN KEY (userID) REFERENCES first_table(id)
+                )";
+            $GLOBALS['$con']->query($sql) or die($GLOBALS['$con']->error);
+            }
+            
+            //Order details table
+            $query = $GLOBALS['$con']->query("SHOW TABLES LIKE 'ORDERS_DETAILS'");
+            if($query->num_rows == 0){
+                $sql = "CREATE TABLE ORDERS_DETAILS (
+                        orderID int NOT NULL AUTO_INCREMENT,
+                        productID int NOT NULL,
+                        quantity int NOT NULL,
+                        PRIMARY KEY (orderID, productID),
+                        FOREIGN KEY (productID) REFERENCES imagestable(id)
+                )";
+            $GLOBALS['$con']->query($sql) or die($GLOBALS['$con']->error);
+            }
+        ?>          
     </body>
 </html>
 
---
+++ sql/addProductsToDB.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+select orderID from orders where userID = AND status = cart
+        if (no results)
+            add entry and generate the order id 
+            use this order id to enter productID to order_details along with quantity
+            
+        else
+            select productID from order_details where productID = passed value
+                if(no results)
+                    
+
